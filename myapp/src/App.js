f2635229291bd573614787f044b7ae5ed14913c3
imramesh222 <imrameshrawat@gmail.com>
2024-06-23 18:14:40 +0545

    React using

---
+++ myapp/src/App.js
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <div>
+     <h1>Hello</h1>
+     <img src="images/b2.jpg" alt="" width='300px'/>
+    </div>
+  );
+}
+
+export default App;
---
+++ myapp/src/First.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const First = () => {
+  return (
+    <div>
+      <h1>Ramesh</h1>
+      <img src='images/b3.jpg' width="300px"/>
+    </div>
+  )
+}
+
+export default First
