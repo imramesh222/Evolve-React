ca947abcf3a6a026d48f637f28eb73474064a45b
imramesh222 <imrameshrawat@gmail.com>
2024-06-24 14:27:10 +0545

    React using

--- src/Nav.js
+++ src/Nav.js
@@ -1,11 +1,23 @@
-import './Navbar.css';
+import React from 'react';
+
 function Nav() {
   return (
-    <nav className="navbar" style={{display:"flex",fontSize:"40px",textDecoration:"none"}}>
-      <ul className="navbar-list">
-        <li className="navbar-item"><a href="#home">Home</a></li>
-        <li className="navbar-item"><a href="#about">About</a></li>
-      </ul>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#home">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#about">About</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 }
